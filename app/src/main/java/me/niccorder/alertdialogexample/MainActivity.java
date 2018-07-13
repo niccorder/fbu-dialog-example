c7de593bb862badcd24f504103cc2c1bd520e9c6
Nicholas Corder <niccorder25@gmail.com>
2018-07-13 11:52:43 -0700

    initial commit

---
+++ app/src/main/java/me/niccorder/alertdialogexample/MainActivity.java
@@ -0,0 +1,48 @@
+package me.niccorder.alertdialogexample;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setIcon(R.drawable.ic_exit_to_app_black_24dp)
+                        .setMessage("Are you sure you want to log out?")
+                        .setTitle("Log out...")
+                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // You will log out the user, and then
+
+
+                                // this will close the dialog.
+                                dialog.dismiss();
+                            }
+                        })
+                        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // they didn't want to log out, so just close the dialog.
+                                dialog.dismiss();
+                            }
+                        })
+                        .show();
+            }
+        });
+    }
+}
